d2ccefc15fb47744ab48ad4d79e85ff3f8f69f9a
Garin <garin_zh@163.com>
2023-03-01 10:23:08 +0800

    [hold] holding dev for login feature

--- src/tree/features/login/Login.tsx
+++ src/tree/features/login/Login.tsx
@@ -1,23 +1,51 @@
-import React from 'react';
+import { Button, Modal } from 'antd';
+import React, { useState } from 'react';
 
-import { authUI, uiConfig } from '../../../service/auth';
+const Login: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isLoginWithGoogleLoading, setIsLoginWithGoogleLoading] = useState(false);
 
-class Login extends React.Component {
-    componentDidMount() {
-        authUI.start('#firebaseui-auth-container', uiConfig);
-    }
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
 
-    async componentWillUnmount() {
-        await authUI.delete();
-    }
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
 
-    render() {
-        return (
-            <div className="login">
-                <div className="login-form" id="firebaseui-auth-container" />
-            </div>
-        );
-    }
-}
+    const handleCancel = async () => {
+        setIsModalOpen(false);
+    };
+
+    const handleLoginBtnClick = () => {
+        setIsLoginWithGoogleLoading(true);
+        chrome.identity.getAuthToken({ interactive: true }, (token) => {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError);
+            }
+            console.log(token);
+            setIsLoginWithGoogleLoading(false);
+        });
+    };
+
+    return (
+        <div className="login">
+            <Button type="primary" onClick={showModal}>
+                Login
+            </Button>
+            <Modal
+                title="Login"
+                open={isModalOpen}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                footer={null}
+            >
+                <Button onClick={handleLoginBtnClick} loading={isLoginWithGoogleLoading}>
+                    Sign In With Google
+                </Button>
+            </Modal>
+        </div>
+    );
+};
 
 export default Login;
