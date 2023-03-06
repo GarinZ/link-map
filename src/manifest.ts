7d007fcbb6934120635bd586b9e52c8a046262bf
Garin <garin_zh@163.com>
2023-03-06 17:02:36 +0800

    [optimize] replace logo/icon, add description for plugin

--- src/manifest.ts
+++ src/manifest.ts
@@ -5,6 +5,7 @@ import pkg from '../package.json';
 const manifest: Manifest.WebExtensionManifest = {
     name: pkg.displayName,
     version: pkg.version,
+    description: pkg.description,
     manifest_version: 3,
     minimum_chrome_version: pkg.browserslist.split(' ')[2],
     permissions: ['tabs', 'storage', 'activeTab', 'windows', 'downloads'],
@@ -30,11 +31,10 @@ const manifest: Manifest.WebExtensionManifest = {
     action: {
         // default_popup: 'popup.html',
         default_icon: {
-            '16': 'icons/icon-x16.png',
-            '32': 'icons/icon-x32.png',
-            '48': 'icons/icon-x48.png',
-            '64': 'icons/icon-x64.png',
-            '128': 'icons/icon-x128.png',
+            '16': 'icons/x16.png',
+            '32': 'icons/x32.png',
+            '48': 'icons/x48.png',
+            '128': 'icons/x128.png',
         },
     },
     options_ui: {
@@ -42,11 +42,10 @@ const manifest: Manifest.WebExtensionManifest = {
         open_in_tab: true,
     },
     icons: {
-        '16': 'icons/icon-x16.png',
-        '32': 'icons/icon-x32.png',
-        '48': 'icons/icon-x48.png',
-        '64': 'icons/icon-x64.png',
-        '128': 'icons/icon-x128.png',
+        '16': 'icons/x16.png',
+        '32': 'icons/x32.png',
+        '48': 'icons/x48.png',
+        '128': 'icons/x128.png',
     },
     // @ts-expect-error type not support
     key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA14QGB1nbCJbeYguBluEQGQOaeQJ8Tln5VMr8LpQuMKlpfKPiJJYwDMQbf1AaRnUAwaC11/0yrjaX3Fpg+iwQNRju7jvEbYdanvrfGd9qwtzgTpU4lNVY6Vk/3DHlmMKIoq80/a1Mwzi3aPDzLNYppSowwDPjCRbzhdC09Juq7xWrpw5oC8l/fei8gCVYwqEt0Ff30wQHHavz0/hbvDjCLs9Jv3dVgp8ZNY4m87+wsyWPhpwU1IVU/z6NujW87VmUYA+Vo1Tvbe/1vqNN5vdkKlwdKOoBXN+4wPV0EacR8vZ65LSrpiSCkR3gxYTnyDjFKPvOi+FbG0i1jyqEsOUd7wIDAQAB',
