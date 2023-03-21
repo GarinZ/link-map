de2c403139216e8116d07743b0ff20f39373f9cc
Garin <garin_zh@163.com>
2023-03-21 12:03:37 +0800

    [security] fix security problemes

--- src/manifest.ts
+++ src/manifest.ts
@@ -49,15 +49,6 @@ const manifest: Manifest.WebExtensionManifest = {
         '48': 'icons/x48.png',
         '128': 'icons/x128.png',
     },
-    // @ts-expect-error type not support
-    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA14QGB1nbCJbeYguBluEQGQOaeQJ8Tln5VMr8LpQuMKlpfKPiJJYwDMQbf1AaRnUAwaC11/0yrjaX3Fpg+iwQNRju7jvEbYdanvrfGd9qwtzgTpU4lNVY6Vk/3DHlmMKIoq80/a1Mwzi3aPDzLNYppSowwDPjCRbzhdC09Juq7xWrpw5oC8l/fei8gCVYwqEt0Ff30wQHHavz0/hbvDjCLs9Jv3dVgp8ZNY4m87+wsyWPhpwU1IVU/z6NujW87VmUYA+Vo1Tvbe/1vqNN5vdkKlwdKOoBXN+4wPV0EacR8vZ65LSrpiSCkR3gxYTnyDjFKPvOi+FbG0i1jyqEsOUd7wIDAQAB',
-    oauth2: {
-        client_id: '1043610828782-3ifjhc7442tk3cd3d2rkvpv2qk25b3bj.apps.googleusercontent.com',
-        scopes: [
-            'https://www.googleapis.com/auth/userinfo.email',
-            'https://www.googleapis.com/auth/userinfo.profile',
-        ],
-    },
 };
 
 export default manifest;
